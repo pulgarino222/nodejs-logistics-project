53cfbe6948abae3aec99c0ba16db0557431cf1fe
pulgarino222 <pulgarinhernandezjuanfelipe@gmail.com>
2024-07-25 12:29:24 -0500

    feat:despues de entender a profundidad como hacer el delete y el post y el put subimos el avance

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import  express from "express";
 import env from "dotenv"
 import errorHandler from "./middleware/error.handlers.js";
+import routerWarehouses from "./routes/warehouses.js";
 
 const app=express()
 env.config()
 
+app.use(express.json());
 app.use(errorHandler)
+app.use("/warehouses",routerWarehouses)
 
 const PORT= process.env.PORT || 3001
 
--- src/routes/warehouses.js
+++ src/routes/warehouses.js
@@ -1,29 +1,62 @@
-import files from "fs"
-import Router from "express"
-import path from "path"
-import  {fileURLToPath}  from "url"
+import fs from "fs"
+import {Router} from "express"//esto es para definir las rutas de la appi
+import path from "path"//esto nos arroja rutas de archivos y directorios
+import  {fileURLToPath}  from "url"//esto simplemente nos ayuda a reconocer la ubicacion actual donde estamos parados
 
-const fs=files
-const router = Router(); // Crea una instancia de Router para definir rutas
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const routerWarehouses = Router(); // Crea una instancia de Router para definir rutas de la api
 const _filename = fileURLToPath(import.meta.url); // Obtiene la ruta del archivo actual
 const _dirname = path.dirname(_filename); // Obtiene el directorio del archivo actual
-const whareHouseFilePath = path.join(_dirname, "../../public/data/warehouses.json"); // Ruta al archivo JSON de wharehouses
+const whareHouseFilePath = path.join(_dirname, `../../public/data/warehouses.json`); // Ruta al archivo JSON de wharehouses
 
 
 // Leer los warehouses desde el archivo
-async function readWharehouses() {
-    try {
-        const WharehousesData =  fs.readFile(whareHouseFilePath); 
-        return JSON.parse(WharehousesData); // Parsea el contenido del archivo y lo devuelve como objeto JSON
-    } catch (error) {
-        throw new Error(`Error en la promesa ${error.message}`); // Maneja errores en la lectura del archivo
+
+const readFiles=()=>{
+    try{
+        const wharehouses=fs.readFileSync(whareHouseFilePath,`utf-8`)
+        return JSON.parse(wharehouses)
+    }catch(err){
+        throw new Error ("ha ocurrido un error leyendo la base de datos de wharehouses",err)
+
     }
 }
 
-console.log(readWharehouses())
+const writeFiles=(wharehouses)=>{
+    fs.writeFileSync(whareHouseFilePath,JSON.stringify(wharehouses,null,2))
+
+}
+
+routerWarehouses.post('/',(req,resp)=>{
+    const wharehouses=readFiles()
+    const newWharehouses={
+        id: wharehouses.length +1,
+        name: req.body.name,
+        location:req.body.location
+    }
+    wharehouses.push(newWharehouses)
+    writeFiles(wharehouses)
+    resp.status(201).json({"message": "wharehouses added successfully", "anime": newWharehouses})
+
+})
+
+
+// const readFileAnimes=()=>{
+//     fs.readFile(whareHouseFilePath,"utf-8",(err,data)=>{
+//         if(data){
+//             return JSON.parse(data)
+//         }else{
+//             throw new error("ha ocurrido un error",err)
+//         }
+//     })
+// }
 
 
 
+
+export default routerWarehouses
 
 
 
